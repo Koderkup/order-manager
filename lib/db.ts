723f92ef3ba7696afdaa79b4ca56f3bacad39a56
Koderkup <koderkup@gmail.com>
2026-02-18 20:00:12 +0300

    fix bug in order loading

--- lib/db.ts
+++ lib/db.ts
@@ -1,14 +1,32 @@
 import mysql from "mysql2/promise";
+import fs from "fs";
+import path from "path";
 
+
+const certPath = path.join(process.cwd(), "certs", "isrgrootx1.pem");
+
+let sslConfig;
+try {
+  sslConfig = {
+    rejectUnauthorized: true,
+    ca: fs.readFileSync(certPath),
+  };
+  console.log("✅ SSL сертификат загружен");
+} catch (error) {
+  console.warn("⚠️ Сертификат не найден, используем стандартную проверку");
+  sslConfig = {
+    rejectUnauthorized: true, 
+  };
+}
 const pool = mysql.createPool({
   host: process.env.NEXT_PUBLIC_HOST,
   port: process.env.NEXT_PUBLIC_PORT
     ? parseInt(process.env.NEXT_PUBLIC_PORT)
-    : 3306,
+    : 4000,
   user: process.env.NEXT_PUBLIC_DATABASE_USER,
   password: process.env.NEXT_PUBLIC_PASSWORD,
   database: process.env.NEXT_PUBLIC_DATABASE_NAME,
-  ssl: { rejectUnauthorized: false },
+  ssl: sslConfig, //ssl: { rejectUnauthorized: false },
   connectTimeout: 20000,
 });
 
