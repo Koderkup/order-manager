36f49620c01ed4a27f2dc217f6898eff4e57a916
Koderkup <koderkup@gmail.com>
2026-04-08 14:44:23 +0300

    add inactive user rout

---
+++ app/inactive/page.tsx
@@ -0,0 +1,22 @@
+import { FiLock } from 'react-icons/fi';
+import Link from 'next/link';
+export default function Forbidden() {
+  return (
+    <div className='flex flex-col items-center justify-center min-h-screen bg-background text-foreground'>
+      <h1 className='text-6xl font-bold mb-4'>403</h1>
+      <p className='text-xl mb-6'>Доступ временно закрыт</p>
+      <p className='text-md text-gray-500 dark:text-gray-400'>
+        <b>Обратитесь к администратору!</b>
+      </p>
+      <Link
+        href='/'
+        className='mt-6 px-4 py-2 flex items-center gap-2 rounded 
+                 bg-[#3E4F5F] text-white font-medium shadow-md 
+                 hover:shadow-lg hover:bg-[#3E4F5F]/80 transition-colors'
+      >
+        <FiLock className='text-xl' />
+        Вернуться на главную
+      </Link>
+    </div>
+  );
+}
